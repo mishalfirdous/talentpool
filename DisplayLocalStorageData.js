516e0c9f9be6b42640d7e602fedb90428af8fabc
mishalfirdous <mishal.firdous@codeninjaconsulting.com>
2023-11-22 11:27:44 +0500

    add sepaerate js file in the file

--- DisplayLocalStorageData.js
+++ DisplayLocalStorageData.js
@@ -12,48 +12,7 @@ function DisplayLocalStorageData() {
     ${parsItem.description}
 
 `;
-
       experienceList.appendChild(experienceEntry);
     });
   }
-  DisplayLocalStorageData();
-
-  document.addEventListener("DOMContentLoaded", function () {
-    const addButton = document.getElementById("addButton");
-    const experienceForm = document.getElementById("experienceForm");
-    const saveButton = document.getElementById("saveButton");
-    const experienceList = document.getElementById("experienceList");
-    //store data in localStorage
-    const dataValue =
-      JSON.parse(localStorage.getItem("experiencedata")) || [];
-
-    addButton.addEventListener("click", function () {
-      experienceForm.style.display = "block";
-    });
-
-    saveButton.addEventListener("click", function () {
-      const companyName = document.getElementById("companyName").value;
-      const startDate = document.getElementById("startDate").value;
-      const endDate = document.getElementById("endDate").value;
-      const description = document.getElementById("description").value;
-      
-      const experienceData = {
-        companyName,
-        startDate,
-        endDate,
-        description,
-      };
-
-      dataValue.push(JSON.stringify(experienceData));
-      localStorage.setItem("experiencedata", JSON.stringify(dataValue));
-      
-      DisplayLocalStorageData();
-
-      document.getElementById("companyName").value = "";
-      document.getElementById("startDate").value = "";
-      document.getElementById("endDate").value = "";
-      document.getElementById("description").value = "";
-
-      experienceForm.style.display = "none";
-    });
-  });
+  DisplayLocalStorageData(); 
