f8c3631c60308e33b9c9eb3b6d0c1e4b4c591d67
mishalfirdous <mishal.firdous@codeninjaconsulting.com>
2023-11-22 12:56:10 +0500

    worked on displaylocalstoragedata function

--- DisplayLocalStorageData.js
+++ DisplayLocalStorageData.js
@@ -1,18 +1,17 @@
-function DisplayLocalStorageData() {
-    const experienceList = document.getElementById("experienceList");
-    const localData = JSON.parse(localStorage.getItem("experiencedata"));
-    localData.forEach((item) => {
-      const parsItem = JSON.parse(item);
+function displayLocalStorageData() {
+  const experienceList = document.getElementById("experienceList");
+  const localData = JSON.parse(localStorage.getItem("experiencedata"));
+  localData.forEach((item) => {
+    const parsItem = JSON.parse(item);
 
-      const experienceEntry = document.createElement("p");
-      experienceEntry.classList.add("experience-entry");
-      experienceEntry.innerHTML = `
+    const experienceEntry = document.createElement("p");
+    experienceEntry.classList.add("experience-entry");
+    experienceEntry.innerHTML = `
    <h2> <strong>${parsItem.companyName}</strong><br></h2>
     ${parsItem.startDate} - ${parsItem.endDate} <br>
     ${parsItem.description}
 
 `;
-      experienceList.appendChild(experienceEntry);
-    });
-  }
-  DisplayLocalStorageData(); 
+    experienceList.appendChild(experienceEntry);
+  });
+}
